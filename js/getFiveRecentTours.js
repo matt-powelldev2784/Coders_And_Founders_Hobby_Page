b0964150215b7391112ccd4e62e273014041969a
Matthew Powell <matt.powell2784@gmail.com>
2022-11-17 20:56:14 +0000

    tour date section

---
+++ js/getFiveRecentTours.js
@@ -0,0 +1,23 @@
+import { tourInfo } from './tourDates.js'
+
+const getNextFiveTourDates = (tourInfo) => {
+  const tourMostRecentDateFirst = tourInfo.sort((a, b) => {
+    return new Date(a.date) - new Date(b.date)
+  })
+
+  const futureTourDates = tourMostRecentDateFirst.filter((tourInfo) => {
+    const today = new Date()
+    const tourDate = new Date(tourInfo.date)
+    return tourDate >= today
+  })
+
+  const nextFiveTourDates = futureTourDates.filter((tourInfo, i) => {
+    if (i < 5) {
+      return tourInfo
+    }
+  })
+
+  return nextFiveTourDates
+}
+
+export const nextFiveTourDates = getNextFiveTourDates(tourInfo)
