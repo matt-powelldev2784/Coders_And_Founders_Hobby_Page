8f8aec3cbcdf9ab49edc78209c05b5f04672b1a1
Matthew Powell <matt.powell2784@gmail.com>
2022-11-18 21:49:41 +0000

    flex containers setup

--- js/addNodesForTours.js
+++ js/addNodesForTours.js
@@ -6,21 +6,21 @@ export const addNodesForTours = (tours) => {
 
     //add date node
     const tourDate = document.createElement('div')
-    tourDate.classList.add('tours_container__ol__li___tour_date')
+    tourDate.classList.add('tours__tour_date')
     const tourDateText = document.createTextNode(tourInfo.date)
     listItem.appendChild(tourDate)
     tourDate.appendChild(tourDateText)
     document.getElementById('tours_container__ol').appendChild(listItem)
 
-    //add city and venie container node
+    //add city and venue container node
     const placeConatiner = document.createElement('div')
-    placeConatiner.classList.add('tours_container__ol__li__place_container')
+    placeConatiner.classList.add('tours_place__container')
     listItem.appendChild(placeConatiner)
     document.getElementById('tours_container__ol').appendChild(listItem)
 
     //add city node
     const tourCity = document.createElement('div')
-    tourCity.classList.add('tours_container__ol__li__tour_city')
+    tourCity.classList.add('tours_place__city')
     const city = tourInfo.city.toUpperCase()
     const tourCityText = document.createTextNode(city)
     placeConatiner.appendChild(tourCity)
@@ -29,7 +29,7 @@ export const addNodesForTours = (tours) => {
 
     //add venue node
     const tourVenue = document.createElement('div')
-    tourVenue.classList.add('tours_container__ol__li__tour_venue')
+    tourVenue.classList.add('tours_place__tour_venue')
     const tourVenueText = document.createTextNode(tourInfo.venue)
     placeConatiner.appendChild(tourVenue)
     tourVenue.appendChild(tourVenueText)
